f53d14fbe98f40c3e818ea939631d8f62543e028
yatree2704 <yatree2704@gmail.com>
2026-01-08 22:37:26 +0530

    change 3

--- src/layout/TopNavbar.jsx
+++ src/layout/TopNavbar.jsx
@@ -1,19 +1,3 @@
-function TopNavbar() {
-  return (
-    <header className="top-navbar">
-      <div className="logo">StudyVerse</div>
-
-      <nav className="top-nav-links">
-        <span>Focus</span>
-        <span>Compiler</span>
-        <span>Progress</span>
-        <span>Settings</span>
-      </nav>
-    </header>
-  );
-}
-
-export default TopNavbar;
 
 import React from 'react';
 import './layout.css';
