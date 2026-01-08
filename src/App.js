445f7028c3a4dbe1fced325c907cdefcb3558ec2
yatree2704 <yatree2704@gmail.com>
2026-01-08 22:33:37 +0530

    change 2

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import './index.css';  // use the name of your CSS file
+
 import AppLayout from "./layout/AppLayout";
 
 function App() {
--- src/layout/TopNavbar.jsx
+++ src/layout/TopNavbar.jsx
@@ -14,3 +14,24 @@ function TopNavbar() {
 }
 
 export default TopNavbar;
+
+import React from 'react';
+import './layout.css';
+
+function TopNavbar() {
+    return (
+        <div className="top-navbar">
+            {/* Existing navbar content */}
+            
+            <div className="top-right-buttons">
+                <button className="top-btn">Focus</button>
+                <button className="top-btn">Compiler</button>
+                <button className="top-btn">Progress</button>
+                <button className="top-btn">Settings</button>
+            </div>
+        </div>
+    );
+}
+
+export default TopNavbar;
+
